46218d1c2749356a749c2fbd72040b12fa0696c6
girishdigge <girishdigge@gmail.com>
2024-07-30 17:04:29 +0530

    8.1 init

---
+++ 8.1-customHooks-Generics-Zod/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const firstElement = (arr) => {
+    return arr[0];
+};
+const ele = firstElement([13, 2, 3]);
+console.log(ele);
---
+++ 8.1-customHooks-Generics-Zod/index.ts
@@ -0,0 +1,7 @@
+type NumberArr = number[];
+const firstElement = (arr: NumberArr) => {
+  return arr[0];
+};
+
+const ele = firstElement([13, 2, 3]);
+console.log(ele);
