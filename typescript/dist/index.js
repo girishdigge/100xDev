9c28bb44986fdb90d614341301695df75bdad321
girishdigge <girishdigge@gmail.com>
2024-08-04 21:44:15 +0530

    classes

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -1,7 +1,34 @@
 "use strict";
-let a = 21;
-console.log(a);
-if (a < 50)
-    a += 10;
-console.log(a);
+class Account {
+    constructor(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+    withdraw(amount) {
+        return this.deduct(amount);
+    }
+    deduct(amount) {
+        console.log(amount);
+        if (amount < this.balance) {
+            this.balance -= amount;
+            return 'your new balance: ' + this.balance;
+        }
+        return 'unsuccessful';
+    }
+    deposit(amount) {
+        if (amount <= 0)
+            throw new Error('invalid amount');
+        this.balance += amount;
+        return ('your account ' +
+            this.id +
+            ' +' +
+            amount +
+            ' deposited. Current balance is : ' +
+            this.balance);
+    }
+}
+const user = new Account(1, 'girish', 521);
+console.log(user.deposit(500));
+console.log(user.withdraw(221));
 //# sourceMappingURL=index.js.map
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,15 +1,41 @@
-type users = {
-  name: string;
-  age: number;
-  occupation?: string;
-};
+class Account {
+  readonly id: number;
+  owner?: string;
+  private balance: number;
+  nickname?: string;
 
-type Bird = {
-  Fly: () => void;
-};
+  constructor(id: number, owner: string, balance: number) {
+    this.id = id;
+    this.owner = owner;
+    this.balance = balance;
+  }
+  withdraw(amount: number): string {
+    return this.deduct(amount);
+  }
+  private deduct(amount: number) {
+    console.log(amount);
+    if (amount < this.balance) {
+      this.balance -= amount;
+      return 'your new balance: ' + this.balance;
+    }
+    return 'unsuccessful';
+  }
 
-type Fish = {
-  Swim: () => void;
-};
+  deposit(amount: number): string {
+    if (amount <= 0) throw new Error('invalid amount');
 
-type DayOfWeek = 'M' | 'T';
+    this.balance += amount;
+    return (
+      'your account ' +
+      this.id +
+      ' +' +
+      amount +
+      ' deposited. Current balance is : ' +
+      this.balance
+    );
+  }
+}
+
+const user = new Account(1, 'girish', 521);
+console.log(user.deposit(500));
+console.log(user.withdraw(221));
