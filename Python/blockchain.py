0b009581431761ab95857f68b2302ba266dca3d0
girishdigge <girishdigge@gmail.com>
2025-03-11 13:04:52 +0530

    blockchain implementation init

---
+++ Python/blockchain.py
@@ -0,0 +1,17 @@
+blockchain=[]
+
+
+def get_last_blockchain_value():
+ return blockchain[-1]    
+
+
+def add_value(transaction_amount,last_transaction=[0]):
+    blockchain.append([last_transaction,transaction_amount])
+   
+
+
+add_value(1)
+add_value(2,get_last_blockchain_value())
+add_value(3,get_last_blockchain_value())
+
+print(blockchain)
