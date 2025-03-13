9ba96099f062924f7124908be788dcb3f442d8f2
girishdigge <girishdigge@gmail.com>
2025-03-13 23:38:23 +0530

    hash and get balance update

--- Python/blockchain.py
+++ Python/blockchain.py
@@ -1,3 +1,7 @@
+from functools import reduce
+import hashlib as hl
+import json
+
 MINING_REWARD=10
 genesis_block={'previous_hash':'',
                'index':0,
@@ -9,21 +13,17 @@ owner='Girish'
 participants={'Girish'}
 
 def hash_block(block):
-   return '-'.join([str(block[key])for key in block])
+   return hl.sha256(json.dumps(block).encode()).hexdigest()
 
 def get_balance(participant):
     tx_sender=[[tx['amount'] for tx in block['transactions'] if tx['sender']== participant] for block in blockchain]
     open_tx_sender=[tx['amount'] for tx in open_transactions if tx['sender']== participant]
     tx_sender.append(open_tx_sender)
-    amount_sent=0
-    for tx in tx_sender:
-        if len(tx)>0:
-           amount_sent +=tx[0]
+    amount_sent=reduce(lambda tx_sum,tx_amt:tx_sum+sum(tx_amt) if len(tx_amt)>0 else tx_sum+0,tx_sender,0)
+    
     tx_recipient=[[tx['amount'] for tx in block['transactions'] if tx['recipient']== participant] for block in blockchain]   
-    amount_received=0
-    for tx in tx_recipient:
-        if len(tx)>0:
-           amount_received +=tx[0]
+    amount_received=reduce(lambda tx_sum,tx_amt:tx_sum+sum(tx_amt) if len(tx_amt)>0 else tx_sum+0,tx_recipient,0)
+    
     return amount_received-amount_sent
     
 def get_last_blockchain_value():
@@ -57,6 +57,7 @@ def add_transaction(recipient,sender=owner,amount=1.0):
 def mine_block():
    last_block=blockchain[-1]
    hashed_block= hash_block(last_block)
+   print(hashed_block)
    reward_transaction={
       'sender':'MINING',
       'recipient':owner,
@@ -148,6 +149,6 @@ while waiting_for_input:
       print_blockchain_element()
       print("Blockchain contaminated")  
       break
-   print(get_balance('Girish'))
+   print('Balance of {}: {:6.2f}'.format('Girish',get_balance('Girish')))
 else:
    print('User left!!!!!!!!!!')   
