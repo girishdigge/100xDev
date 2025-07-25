02f005478e99c8f78ee582e7c63e12e368a1fb70
girishdigge <girishdigge@gmail.com>
2025-07-25 23:00:21 +0530

    go revisit

--- Lang/Go/helloWorld.go
+++ Lang/Go/helloWorld.go
@@ -4,6 +4,6 @@ import "fmt"
 func main(){
 	const name="Girish";
 	const age=21;
-	fmt.Printf("Hello World🌍, Let's Goooooo!,%s is here,I am forever %d  \v %v %b.",name,age,name,age)
+	fmt.Printf("Hello World🌍, Let's Goooooo!,%s here,I am forever %d  \v %v %b.",name,age,name,age)
 	fmt.Printf(`hey yoo'o ♥️ %d  `,age)
 }
