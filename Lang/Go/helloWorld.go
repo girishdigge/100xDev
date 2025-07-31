bc9ae82c4bb141cc59a076cb146bbe6ca4aeb5b4
girishdigge <girishdigge@gmail.com>
2025-07-31 23:50:59 +0530

    basic http in golang

--- Lang/Go/helloWorld.go
+++ Lang/Go/helloWorld.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+) 
 func main(){
 	const name="Girish";
 	const age=21;
 	fmt.Printf("Hello World🌍, Let's Goooooo!,%s here,I am forever %d  \v %v %b.",name,age,name,age)
-	fmt.Printf(`hey yoo'o ♥️ %d  `,age)
+	fmt.Print(runtime.GOOS)
+	fmt.Printf(`hey yoo'o  %d  `,age)
 }
