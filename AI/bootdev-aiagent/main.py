1bce6db6f6c15a66bd4b3df60ae8189c1e53149d
girishdigge <girishdigge@gmail.com>
2025-09-04 12:37:22 +0000

    bootdev ai agent

---
+++ AI/bootdev-aiagent/main.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) < 2:
+        print("i need prompt!")
+        sys.exit(1)
+    prompt = sys.argv[1]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=prompt
+    )
+
+    print(response.text)
+    print(f"prompt tokens:{response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
+
+
+main()
