af10c444a071e736a4697ff81ec4d0fe9af784f9
girishdigge <girishdigge@gmail.com>
2026-06-27 07:37:33 +0000

    psql migrations

---
+++ Cohort 1/psql-migrations/migrations/1782545154761_rename-contents-to-body.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.sql(`ALTER TABLE comments
+        RENAME COLUMN contents to body;`);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.sql(`ALTER TABLE comments
+        RENAME COLUMN body to contents;`);
+};
